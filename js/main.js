1c68137a7254a2d2cd017d061900c2110f356a12
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2017-12-29 13:24:27 +1100

    added function to get and change attributes of elements

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+  function changeAttr(el, attr, attrProperty){
+    var el = document.querySelector(el);
+    el.setAttribute(attr, attrProperty)
+  }//gets the element. changes attribute, Style in the case of css.
+
+  changeAttr('#draw', 'style', 'color:red;')
+
 }, false);
