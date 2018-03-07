8b8eb86682d4524ee2cfcbdf3096d771a1ba1647
James Theo <jamestheo@Jamess-MacBook-Pro-2.local>
2018-03-08 09:43:41 +1100

    added some css and html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 
   function changeAttr(el, attr, attrProperty){
-    var el = document.querySelector(el);
-    el.setAttribute(attr, attrProperty)
+    document.querySelector(el).setAttribute(attr, attrProperty);
   }//gets the element. changes attribute, Style in the case of css.
-
   // changeAttr('#draw', 'style', 'color:red;') EXAMPLE CODE USAGE
-
 }, false);
