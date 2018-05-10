ebd3cdce76f9d36ea1a12b9cf87ca32b5e508756
James Theo <jamestheo@Jamess-MacBook-Pro-3.local>
2018-05-10 14:23:26 +1000

    updated version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,95 @@
-document.addEventListener('DOMContentLoaded', function(){
 
+  var circles = [];
+  var velocityScale = 1
+
+  var setup = function(){
+
+    createCanvas( windowWidth, windowHeight ); // window.innerWidth
+    background(0); // black background; could also use RGB: background(0, 0, 0);
+    colorMode(HSB, 255); // Use Hue Saturation Brightness, with a range of 0-255 for each
   
+    noStroke(); // Don't draw a border on shapes  
+    textSize(24);  // If you want to use text()
+    // blendMode(LIGHTEST);
+  }; // setup function sets up the initial properties of our canvas
+
+  var draw = function(){
+
+    background(0) // resets background colour to black at each frame.
+
+    // ellipse(windowWidth/2, windowHeight/2, 80, 80);
+    // rect(mouseX, mouseY, 80, 80);
+    // text('p5 is cool', 100, 200)
+
+    // fill(250, 250, 150)
+
+
+
+    // if (keyIsDown(SHIFT) || mouseIsPressed) {
+    //   ellipse(mouseX, mouseY, 80, 80)
+    //   // rect(mouseX, mouseY, 80, 80);
+
+    //   fill(random(250), 150, 250)
+    // } // First example
+
+
+    // if (keyIsDown(SHIFT) || mouseIsPressed) {
+    //   var hue = map(mouseX, 0, windowWidth, 0, 255)
+    //   ellipse(mouseX, mouseY, 50, 50)
+    //   stroke(hue, 255, 255)
+    //   line(mouseX, mouseY, mouseX + 200, mouseY + 200)
+    // } // Second Example
+
+    // if (keyIsDown(SHIFT) || mouseIsPressed) {
+    //   var height = random(50, 50)
+    //   var width = random(50, 50)
+
+    //   triangle(
+    //     mouseX, mouseY,
+    //     mouseX - width, mouseY + height,
+    //     mouseX + width, mouseY + height
+    //   )
+
+    //   var size = random(50, 50)
+
+    //   ellipse(random(windowWidth), random(windowHeight), size, size)
+    // } // Third Example
+
+
+     if (keyIsDown(SHIFT) || mouseIsPressed) {
+      var hue = map(mouseX, 0, windowWidth, 0, 255) 
     
-}, false);
+
+     var circle = {
+       velocityX: random(-10, 10),
+       velocityY: random(-10, 10),
+       x: mouseX,
+       y: mouseY,
+       hue: hue,
+       bright: 255
+     };
+
+     circles.push(circle)
+    } // keypress
+
+    for (let i = 0; i < circles.length; i++) {
+      var c = circles[i]
+
+      c.x += c.velocityX * velocityScale
+      c.y += c.velocityY * velocityScale
+
+      if (c.x >= windowWidth || c.x <= 0) {
+        c.velocityX *= -1
+      }
+      if (c.y >= windowHeight || c.y <= 0) {
+        c.velocityY *= -1
+      }
+
+      // c.bright--
+      fill(c.hue, 155, c.bright)
+      ellipse(c.x, c.y, random(80), random(80))
+      // text('p5 is cool', c.x, c.y)
+
+    } // Fourth Example
+  } //draw function called at every frame.
+
